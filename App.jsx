f644543c2527d89de2d06680dd74d5eb79426637
Edidiong Etuk <edidiongmichael08@gmail.com>
2022-03-15 22:27:16 +0100

    nothing showing
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- App.jsx
+++ App.jsx
@@ -3,46 +3,62 @@ import { ethers } from "ethers";
 import "./App.css";
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
     /*
-    * First make sure we have access to window.ethereum
-    */
-    const { ethereum } = window;
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
+  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
-}
 
-  // /*
-  // * This runs our function when the page loads.
-  // */
-
+  /*
+  * This runs our function when the page loads.
+  */
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
-  
-return (
-  <div className="mainContainer">
 
-    <div className="dataContainer">
-      <div className="header">
-      🤓 Hey there Spotify Lover!
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+        🤓 Hey there Spotify Lover!
+        </div>
+
+        <div className="bio">
+        I am Eddie and I do Software Engineering (mostly Infrastructure stuff) at Tellie. I also love Spotify!! Connect your Ethereum wallet and drop a like!
+        </div>
+
+        <button className="likeButton" onClick={null}>
+          Like This Page 👍🏼
+        </button>
       </div>
-
-      <div className="bio">
-      I am Eddie and I do Software Engineering (mostly Infrastructure stuff) at Tellie. I also love Spotify!! Connect your Ethereum wallet and drop a like!
-      </div>
-
-      <button className="likeButton" onClick={wave}>
-        Like This Page 👍🏼
-      </button>
     </div>
-  </div>
-);
+  );
 }
 
 export default App
