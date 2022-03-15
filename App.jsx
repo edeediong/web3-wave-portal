7c5b79357009d28fce2d4636e541758f331952e3
Edidiong Etuk <edidiongmichael08@gmail.com>
2022-03-16 00:09:07 +0100

    fixed indentations
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- App.jsx
+++ App.jsx
@@ -5,6 +5,9 @@ import abi from "./utils/MusicLover.json";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  /**
+   * Create a varaible here that holds the contract address after you deploy!
+   */
   const contractAddress = "0x7316225a2fC73A58a8353CdeA3d91E0b00ce8900";
   const contractABI = abi.abi;
   
@@ -36,39 +39,50 @@ const App = () => {
     }
   }
 
-  /**
-  * Implement your connectWallet method here
-  */
   const connectWallet = async () => {
-    const like = async () => {
-        try {
-          const { ethereum } = window;
-    
-          if (ethereum) {
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const signer = provider.getSigner();
-            const musicLoverContract = new ethers.Contract(contractAddress, contractABI, signer);
-    
-            /*
-            * Execute the actual wave from your smart contract
-            */
-            const likeTxn = await musicLoverContract.like();
-            console.log("Mining...", likeTxn.hash);
-    
-            await likeTxn.wait();
-            console.log("Mined -- ", likeTxn.hash);
-    
-            let count = await musicLoverContract.getTotalLikes();
-            console.log("Retrieved total like count...", count.toNumber());
-          } else {
-            console.log("Ethereum object doesn't exist!");
-          }
-        } catch (error) {
-          console.log(error);
-        }
-    }
+    try {
+      const { ethereum } = window;
 
-    
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const like = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const musicLoverContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await musicLoverContract.getTotalLikes();
+        console.log("Retrieved total like count...", count.toNumber());
+
+        const likeTxn = await musicLoverContract.like();
+        console.log("Mining...", likeTxn.hash);
+
+        await likeTxn.wait();
+        console.log("Mined -- ", likeTxn.hash);
+
+        count = await musicLoverContract.getTotalLikes();
+        console.log("Retrieved total like count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {
@@ -90,9 +104,6 @@ const App = () => {
           Like This Page 👍🏼
         </button>
 
-        {/*
-        * If there is no currentAccount render this button
-        */}
         {!currentAccount && (
           <button className="likeButton" onClick={connectWallet}>
             Connect Wallet
